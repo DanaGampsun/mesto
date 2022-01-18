7988cd6ab6b43c9b426f16f30f1631ac4ce1eece
Лидия Трощинская <lidiya.trosh@yandex.ru>
2022-01-19 00:00:52 +0300

    fix: Pop-up JS

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,7 @@ const OpenProfileButton = document.querySelector(".profile__button")
 const popup = document.querySelector(".popup")
 const popupCloseButton = document.querySelector(".popup__button-close")
 
+/*Вешаем обработчик событий*/
 function openPopup() {
     popup.classList.add('popup_opened')
 }
@@ -12,21 +13,39 @@ function closePopup() {
     popup.classList.remove('popup_opened')
 }
 
+/*Добавляем логику в обработчики*/
 OpenProfileButton.addEventListener("click", openPopup)
 popupCloseButton.addEventListener("click", closePopup)
-console.log(OpenProfileButton)
 
-/*let popupInputName = document.getElementById(".profile_title")
-let popupInputAboutMyself = document.getElementById(".profile-name")
-
-const photoCardLike = document.querySelectorAll(".photo__card-like")*/
-
-
-
-function openPopup() {
-    popup.classList.add("popup_opened")
-}
-
-popupCloseButton.addEventListener("click", function() {
-    console.log("header button clicked")
+popup.addEventListener("click", function(event){
+    if(event.target === event.currentTarget) {
+        closePopup()
+    }
 })
+
+
+// Находим форму в DOM
+let formElement = document.querySelector(".popup__form")
+    let nameInput = document.querySelector(".popup__input_name")
+    let jobInput = document.querySelector(".popup__input_about_myself")
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+    function formSubmitHandler (evt) {
+        evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+        // Так мы можем определить свою логику отправки.
+        // О том, как это делать, расскажем позже.
+
+        // Получите значение полей jobInput и nameInput из свойства value
+
+        // Выберите элементы, куда должны быть вставлены значения полей
+
+        // Вставьте новые значения с помощью textContent
+    }
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+
