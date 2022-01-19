67be980462899aa557e783bcaeee7b17c2eaa4a7
Лидия Трощинская <lidiya.trosh@yandex.ru>
2022-01-20 00:15:04 +0300

    feat: Done JS

--- scripts/index.js
+++ scripts/index.js
@@ -4,18 +4,35 @@ const OpenProfileButton = document.querySelector(".profile__button")
 const popup = document.querySelector(".popup")
 const popupCloseButton = document.querySelector(".popup__button-close")
 
-/*Вешаем обработчик событий*/
+let formElement = document.querySelector(".popup__form")
+let PopupNameInput = document.querySelector(".popup__input_name")
+let PopupInputAboutMyself = document.querySelector(".popup__input_about_myself")
+let InputName = document.querySelector(".profile__title")
+let InputAboutMySelf = document.querySelector(".profile__subtitle")
+
+
+
 function openPopup() {
     popup.classList.add('popup_opened')
+
+    PopupNameInput.value = InputName.textContent
+    PopupInputAboutMyself.value = InputAboutMySelf.textContent
 }
 
 function closePopup() {
     popup.classList.remove('popup_opened')
 }
 
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    InputName.textContent = PopupNameInput.value;
+    InputAboutMySelf.textContent = PopupInputAboutMyself.value;
+    closePopup()
+}
 /*Добавляем логику в обработчики*/
 OpenProfileButton.addEventListener("click", openPopup)
 popupCloseButton.addEventListener("click", closePopup)
+formElement.addEventListener("submit", formSubmitHandler)
 
 popup.addEventListener("click", function(event){
     if(event.target === event.currentTarget) {
@@ -23,29 +40,6 @@ popup.addEventListener("click", function(event){
     }
 })
 
-
-// Находим форму в DOM
-let formElement = document.querySelector(".popup__form")
-    let nameInput = document.querySelector(".popup__input_name")
-    let jobInput = document.querySelector(".popup__input_about_myself")
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-    function formSubmitHandler (evt) {
-        evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-        // Так мы можем определить свою логику отправки.
-        // О том, как это делать, расскажем позже.
-
-        // Получите значение полей jobInput и nameInput из свойства value
-
-        // Выберите элементы, куда должны быть вставлены значения полей
-
-        // Вставьте новые значения с помощью textContent
-    }
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
 
 
 
