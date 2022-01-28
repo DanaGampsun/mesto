c9ca768f250caee2ddab81921b7de0a359bf44df
Лидия Трощинская <lidiya.trosh@yandex.ru>
2022-01-28 21:50:21 +0300

    fix: Fixed adaptive layout

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,21 @@
 "use strict";
 /*Сохраняем ссылки на все нужные элементы*/
-const OpenProfileButton = document.querySelector(".profile__button")
+const openProfileButton = document.querySelector(".profile__button")
 const popup = document.querySelector(".popup")
 const popupCloseButton = document.querySelector(".popup__button-close")
 
 let formElement = document.querySelector(".popup__form")
-let PopupNameInput = document.getElementById("name")
-let PopupInputAboutMyself = document.getElementById("job")
-let InputName = document.querySelector(".profile__title")
-let InputAboutMySelf = document.querySelector(".profile__subtitle")
+let popupNameInput = document.getElementById("name")
+let popupInputAboutMyself = document.getElementById("job")
+let inputName = document.querySelector(".profile__title")
+let inputAboutMySelf = document.querySelector(".profile__subtitle")
 
 
 function openPopup() {
     popup.classList.add('popup_opened')
 
-    PopupNameInput.value = InputName.textContent
-    PopupInputAboutMyself.value = InputAboutMySelf.textContent
+    popupNameInput.value = inputName.textContent
+    popupInputAboutMyself.value = inputAboutMySelf.textContent
 }
 
 function closePopup() {
@@ -24,21 +24,17 @@ function closePopup() {
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    InputName.textContent = PopupNameInput.value;
-    InputAboutMySelf.textContent = PopupInputAboutMyself.value;
+    inputName.textContent = popupNameInput.value;
+    inputAboutMySelf.textContent = popupInputAboutMyself.value;
     closePopup()
 }
 
 /*Добавляем логику в обработчики*/
-OpenProfileButton.addEventListener("click", openPopup)
+openProfileButton.addEventListener("click", openPopup)
 popupCloseButton.addEventListener("click", closePopup)
 formElement.addEventListener("submit", formSubmitHandler)
 
-popup.addEventListener("click", function (event) {
-    if (event.target === event.currentTarget) {
-        closePopup()
-    }
-})
+
 
 
 
